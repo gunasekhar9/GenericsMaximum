eb6a9071ee8793afe82d03f84e2a5e1f8b8f2685
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-18 21:03:17 +0530

    [gunasekhar9] add : Maximum Float is Calculated

--- GenericsMaximum/GenericsMaximum/Program.cs
+++ GenericsMaximum/GenericsMaximum/Program.cs
@@ -10,12 +10,12 @@ namespace GenericsMaximum
             while (flag)
             {
                 Console.WriteLine("Welcome To Generic Maximum Problems");
-                Console.WriteLine("Enter the Program that want to be executed : \n 1. Maximum Integer \n 2.Exit");
+                Console.WriteLine("Enter the Program that want to be executed : \n 1. Maximum Integer \n 2. Maximum Float \n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("Enter First number, Second number, Third number : ");
+                        Console.WriteLine("Enter First number, Second number, Third number to check : ");
                         int first = Convert.ToInt32(Console.ReadLine());
                         int second = Convert.ToInt32(Console.ReadLine());
                         int third = Convert.ToInt32(Console.ReadLine());
@@ -23,6 +23,14 @@ namespace GenericsMaximum
                         Console.WriteLine("The Maximum Number is : " + result);
                         break;
                     case 2:
+                        Console.WriteLine("Enter First number, Second number, Third number to check : ");
+                        double firstNum = Convert.ToDouble(Console.ReadLine());
+                        double secondNum = Convert.ToDouble(Console.ReadLine());
+                        double thirdNum = Convert.ToDouble(Console.ReadLine());
+                        double res = MaximumComputation.FindMax(firstNum, secondNum, thirdNum);
+                        Console.WriteLine("The Maximum Number is : " + res);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
